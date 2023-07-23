4b28a8ae93dcd1d6a50c2eaabc625b8a460774d6
Yash <yashbhajbhuje67@gmail.com>
2023-07-23 15:34:26 +0530

    Added Main calculator functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [answer, setAnswer] = useState(0);
-  const [expression, setExpression] = useState("323x232");
-  const [tempAns, setTempAns] = useState(0);
-  const [finalAns, setFinalAns] = useState(0);
-  const [tempValue, setTempvalue] = useState("");
-  const [sign, setSign] = useState("+");
+  const [expression, setExpression] = useState('');
+  const [tempAns, setTempAns] = useState(0);  
+  const inputRef = useRef(null);
 
-  const clearScreen = () => {
-    setSign("+");
+  const clearAllScreen = () => {
+    setExpression('');
     setTempAns(0);
+    inputRef.current.focus();
   };
 
-  const changeSign = (e) => {
-    setSign(e.target.id);
-  };
-
-  const changeToNegative = () => {
-    setTempAns(-1 * tempAns);
-  };
-  const storeValue = (e) =>{
-    console.log(e.target.value());
+  const handleButton  = (num) =>{
+    setExpression(expression+num);
+    inputRef.current.focus();
+  }
+  
+  const clearScreen = ()=>{
+    setExpression('');
+    inputRef.current.focus();
   }
 
-  const calc = () => {};
+  const storeValue = (e) =>{
+    const allowedCharsRegex = /[0-9+\-*/\s]/;
+    const key = e.key;
+    if (allowedCharsRegex.test(key)) {
+      setExpression(expression + key);
+    } else if (key === 'Enter' || key=== "=" ) {
+      calc();
+    } else {
+      e.preventDefault();
+    }
+  }
+
+  const calc = () => {
+    try{
+      var result = eval(expression);
+      setTempAns(result);
+    }
+    catch(err){
+      alert(`Invalid Input ${expression}`)
+    }
+    inputRef.current.focus();
+  };
 
   return (
     <div className="main">
-      <input className="exp" onChange={(e)=>{storeValue(e)}}/>
+      <input className="exp" value={expression} ref={inputRef} onKeyDown={(e)=>{storeValue(e)}} autoFocus/>
       <div className="ans">
         <div>Ans</div>
         <div></div>
@@ -37,40 +55,38 @@ function App() {
       </div>
 
       <div className="line">
+        <div className="a" onClick={() => {clearAllScreen();}}>AC</div>
         <div className="a" onClick={() => {clearScreen();}}>C</div>
-        <div className="a" onClick={() => {changeToNegative();}}>+/-</div>
         <div className="a" id="%" onClick={() => setTempAns(tempAns / 100)}>%</div>
-        <div className="a" id="/"onClick={(e) => {changeSign(e);}}>÷</div>
+        <div className="a" id="/" onClick={() => {handleButton('/')}}>÷</div>
       </div>
 
       <div className="line">
-        <div className="a" id="7" onClick={(e)=>{}}>7</div>
-        <div className="a" id="8">8</div>
-        <div className="a" id="9">9</div>
-        <div className="a" id="*">X</div>
+        <div className="a" id="7" onClick={()=>{handleButton(7)}}>7</div>
+        <div className="a" id="8" onClick={()=>{handleButton(8)}}>8</div>
+        <div className="a" id="9" onClick={()=>{handleButton(9)}}>9</div>
+        <div className="a" id="*" onClick={()=>{handleButton('*')}}>X</div>
       </div>
 
       <div className="line">
-        <div className="a" id="4">4</div>
-        <div className="a" id="5">5</div>
-        <div className="a" id="6">6</div>
-        <div className="a" id="-">-</div>
+        <div className="a" id="4" onClick={()=>{handleButton(4)}}>4</div>
+        <div className="a" id="5" onClick={()=>{handleButton(5)}}>5</div>
+        <div className="a" id="6" onClick={()=>{handleButton(6)}}>6</div>
+        <div className="a" id="-" onClick={()=>{handleButton('-')}}>-</div>
       </div>
 
       <div className="line">
-        <div className="a" id="1">1</div>
-        <div className="a" id="2">2</div>
-        <div className="a" id="3">3</div>
-        <div className="a" id="+">
-          +
-        </div>
+        <div className="a" id="1" onClick={()=>{handleButton(1)}}>1</div>
+        <div className="a" id="2" onClick={()=>{handleButton(2)}}>2</div>
+        <div className="a" id="3" onClick={()=>{handleButton(3)}}>3</div>
+        <div className="a" id="+" onClick={()=>{handleButton('+')}}>+</div>
       </div>
 
       <div className="line1">
-        <div className="a" id="0">0</div>
-        <div className="a">.</div>
-        <div className="a">Ans</div>
-        <div className="a">=</div>
+        <div className="a" id="0" onClick={()=>{handleButton(0)}}>0</div>
+        <div className="a" onClick={()=>{handleButton('.')}}>.</div>
+        <div className="a" onClick={()=>{handleButton(tempAns)}}>Ans</div>
+        <div className="a" onClick={()=>{calc()}} >=</div>
       </div>
     </div>
   );
